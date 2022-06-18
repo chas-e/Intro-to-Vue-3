7d84e8046d6e0ca03d0a138a1fe4294df0c0fcd3
chas-e <charles@chasengineering.dev>
2022-06-18 17:14:26 -0400

    🚧 Add code through lesson 4

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             description: 'Lovely, gender-neutral socks in green    and blue.',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://github.com/Code-Pop/Intro-to-Vue-3',
+            inventory: 8,
+            onSale: false,
         }
     }
 })
