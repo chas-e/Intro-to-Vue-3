83b07fd278f419d03ad45fa53a18fa24501ff504
chas-e <charles@chasengineering.dev>
2022-06-18 17:26:12 -0400

    🚧 Add code through lesson 5

--- main.js
+++ main.js
@@ -7,6 +7,27 @@ const app = Vue.createApp({
             url: 'https://github.com/Code-Pop/Intro-to-Vue-3',
             inventory: 8,
             onSale: false,
+            details: [
+                '50% cotton',
+                '30% wool',
+                '20% polyester',
+            ],
+            variants: [
+                {
+                    id: 2234,
+                    color: 'green',
+                },
+                {
+                    id: 2235,
+                    color: 'blue',
+                },
+            ],
+            sizes: [
+                'small',
+                'medium',
+                'large',
+                'x-large',
+            ],
         }
     }
 })
